c9d5e8270905ec5518ac3d30378d7dd63dbd80f0
luiizsps <lsps1019@gmail.com>
2023-04-01 19:48:39 -0300

    create spring boot project

---
+++ projeto_main/src/main/java/br/edu/ufrb/gcet236/grupo_fornecedores/GrupoFornecedoresApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufrb.gcet236.grupo_fornecedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoFornecedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoFornecedoresApplication.class, args);
+	}
+
+}
---
+++ projeto_main/src/test/java/br/edu/ufrb/gcet236/grupo_fornecedores/GrupoFornecedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufrb.gcet236.grupo_fornecedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupoFornecedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
