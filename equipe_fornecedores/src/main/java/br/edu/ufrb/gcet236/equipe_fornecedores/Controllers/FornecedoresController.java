070b008ff22f88aba45260ce5acc35f1addc85c5
Luiz Passos <lsps1019@gmail.com>
2023-04-09 21:54:28 -0300

    Update FornecedoresController.java

--- equipe_fornecedores/src/main/java/br/edu/ufrb/gcet236/equipe_fornecedores/Controllers/FornecedoresController.java
+++ equipe_fornecedores/src/main/java/br/edu/ufrb/gcet236/equipe_fornecedores/Controllers/FornecedoresController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import br.edu.ufrb.gcet236.equipe_fornecedores.Fornecedor;
 import br.edu.ufrb.gcet236.equipe_fornecedores.ListaFornecedor;
 
@@ -28,9 +28,9 @@ public class FornecedoresController {
     return lista.getListaDeFornecedores();
   }
 
-  @PostMapping(value = "/search/cnpj")
-  public Object searchByCNPJ(@RequestBody String cnpj) {
-    Object resultado = lista.buscaPorCNPJ(cnpj);
+  @GetMapping(value = "/search/cnpj:{cnpj}")
+  public Fornecedor searchByCNPJ(@PathVariable String cnpj) {
+    Fornecedor resultado = lista.buscaPorCNPJ(cnpj);
 
     return resultado;
   }
@@ -40,28 +40,29 @@ public class FornecedoresController {
     return lista.listNameAndCnpj();
   }
 
-  @PostMapping(value = "/search/name")
-  public Object searchByName(@RequestBody String name) {
-    Object resultado = lista.buscaPorNome(name);
+  @GetMapping(value = "/search/name:{name}")
+  public Fornecedor searchByName(@PathVariable String name) {
+    Fornecedor resultado = lista.buscaPorNome(name);
 
     return resultado;
   }
 
-  @PostMapping(value = "/search/part")
-  public ArrayList<Fornecedor> searchByPartOfName(@RequestBody String name) {
+  @GetMapping(value = "/search/part:{name}")
+  public ArrayList<Fornecedor> searchByPartOfName(@PathVariable String name) {
     return lista.buscaPorParteDoNome(name);
   }
 
-  @PutMapping(value = "/update/{index}")
-  public void atualizarFornecedor(@PathVariable int index, @RequestBody Fornecedor fornecedor){
-    lista.alterarDados(index, fornecedor);
-  }
- /* @DeleteMapping(value = "/remove/cnpj")
+  @DeleteMapping(value = "/remove/cnpj:{cnpj}")
   public ArrayList<Fornecedor> deleteByCNPJ(@PathVariable String cnpj) {
     int index = lista.buscaPorCNPJEPosicao(cnpj);
     lista.removeByIndex(index);
 
     return lista.getListaDeFornecedores();
   }
-*/
+}
+
+  @PutMapping(value = "/update/{index}")
+  public void atualizarFornecedor(@PathVariable int index, @RequestBody Fornecedor fornecedor){
+    lista.alterarDados(index, fornecedor);
+  }
 }
